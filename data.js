9731d758fc1379a0707057792ab2f93e2a752c46
Alexdander <ms_pros@bk.ru>
2019-01-28 16:07:14 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-",
-      "name": "Гардероб",
+      "name": "Прихожая",
       "levels": [
         {
           "tileSize": 256,
@@ -16,20 +16,30 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": -2.0880534783575637,
-        "pitch": -0.15290970000582504,
-        "fov": 1.3729094996294073
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -2.0765328172648907,
-          "pitch": 0.01346624296051857,
+          "yaw": 0.35073872739637757,
+          "pitch": 0.09485794961467064,
           "rotation": 0,
-          "target": "1-"
+          "target": "3-"
+        },
+        {
+          "yaw": -0.33545493436442975,
+          "pitch": 0.11274708114704701,
+          "rotation": 4.71238898038469,
+          "target": "6-"
         }
       ],
       "infoHotspots": []
@@ -54,25 +64,358 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.7504915783575701,
+        "pitch": 0.04464652448891826,
+        "fov": 1.3729094996294073
       },
       "linkHotspots": [
         {
-          "yaw": -2.7659354288938847,
-          "pitch": -0.0034178698863698287,
-          "rotation": 6.283185307179586,
-          "target": "0-"
+          "yaw": -2.0584997206257505,
+          "pitch": 0.08142649737952112,
+          "rotation": 0,
+          "target": "3-"
         }
       ],
       "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Гардеробная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.006347184976021936,
+        "pitch": 0.1130624931216957,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0391819485397704,
+          "pitch": -0.06954012744675708,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Гостиная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.5974040702430212,
+        "pitch": 0.048492833462384866,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7693825536395043,
+          "pitch": 0.5473328445350916,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -1.0934277377184571,
+          "pitch": 0.0064645114519272795,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.5624404103992404,
+          "pitch": -0.020693949453569616,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.9901800052537117,
+          "pitch": -0.007783294715121514,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.459843939339269,
+          "pitch": 0.010328769930580961,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.5229379040009619,
+          "pitch": 0.016041236703820516,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6774573916725366,
+          "pitch": 0.673926377409078,
+          "title": "Динс 140",
+          "text": "https://www.divan.ru/product/modulnyj-divan-dins-velvet-yellow"
+        },
+        {
+          "yaw": 0.6875543271731495,
+          "pitch": 0.3293133762597229,
+          "title": "Хавьер Luna",
+          "text": "https://www.divan.ru/product/divan-haver-funky-luna"
+        }
+      ]
+    },
+    {
+      "id": "4--",
+      "name": "Включить свет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.5432198783489213,
+        "pitch": 0.4549351764551197,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7339237200762554,
+          "pitch": 0.5485729920039653,
+          "rotation": 0,
+          "target": "7--"
+        },
+        {
+          "yaw": -1.0852956387944364,
+          "pitch": 0.03718282666594419,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.557111983307075,
+          "pitch": -0.010464225270425231,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.0005447869018376,
+          "pitch": -0.003965678369084458,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.4636929527116678,
+          "pitch": -0.0046641667378626295,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.4993067025691698,
+          "pitch": 0.07387243622226336,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6608103848162123,
+          "pitch": 0.6784275982815675,
+          "title": "Динс 140",
+          "text": "https://www.divan.ru/product/modulnyj-divan-dins-velvet-yellow"
+        },
+        {
+          "yaw": 0.7356231754588727,
+          "pitch": 0.34738669423771107,
+          "title": "Хавьер Luna",
+          "text": "https://www.divan.ru/product/divan-haver-funky-luna"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "Кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.5312429639855516,
+        "pitch": 0.05762766238783712,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.116660417781169,
+          "pitch": -0.024880641062130948,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "Спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.3128167043958854,
+        "pitch": 0.09487585973048951,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5965489108189779,
+          "pitch": 0.0363468712268471,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "Выключить свет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.5615759164393292,
+        "pitch": 0.43751437163885853,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7739443413701412,
+          "pitch": 0.546311206382196,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -1.0986304496373318,
+          "pitch": 0.05826925052668841,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.5540236084506622,
+          "pitch": 0.02732321002238436,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.9845268698252578,
+          "pitch": 0.03382352287779966,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.543458462306255,
+          "pitch": 0.04820188160250183,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.5114086355112129,
+          "pitch": 0.04970488771925652,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6848813178579931,
+          "pitch": 0.6648838766539296,
+          "title": "Динс 140",
+          "text": "https://www.divan.ru/product/modulnyj-divan-dins-velvet-yellow"
+        },
+        {
+          "yaw": 0.7121140999694422,
+          "pitch": 0.3401868982582581,
+          "title": "Хавьер Luna",
+          "text": "https://www.divan.ru/product/divan-haver-funky-luna"
+        }
+      ]
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
